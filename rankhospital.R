9dbff2b42b36fac5163faa59119bb0601aec0b71
inesvidal <ines.vidal@gmail.com>
2015-04-22 13:33:46 +0200

    tests ok

--- rankhospital.R
+++ rankhospital.R
@@ -16,19 +16,29 @@ rankhospital <- function(state, outcome, num = "best") {
     # cases[[outcome]] alternative to cases$outcome, as $ does not support 
     # variables to store element names
     
-    # Select hospitals in the state
-    state_data <- subset(data, data[["State"]] == state, )
+    # Select hospitals in the state with data for the outcome
+    state_data <- subset(data, data[["State"]] == state & data[[cases[[outcome]]]] != "Not Available", )
+
+    # Prepare indexes for "string" positions in the rank
     position <- list("best" = 1, "worst" = nrow(state_data))
+    # if "best" or "worst" are used
     if (! is.numeric(num)) {
-        num <- position[[num]]
+        rank <- position[[num]]
     } 
+    else {
+        # if the position in the ranking is out of the list
+        if (num > position[["worst"]] ) {
+            return("NA")
+        }
+    }
     
     # Keep hospital with the lowest rate for the desired outcome
     # Order hospitals by outcome value and then by name
     sorted_best_hospitals <- 
         state_data[order(as.numeric(state_data[[cases[[outcome]]]]),state_data[["Hospital.Name"]]),]
+    
     #return the first in the list
-    return(sorted_best_hospitals[[num, 2]])
+    return(sorted_best_hospitals[[rank, 2]])
 }
 
 # >source("rankhospital.R")
