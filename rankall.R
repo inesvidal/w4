7ab293837a459a89d11bbb7e228277417a766fa6
ines vidal <ines@iness-MacBook-Pro.local>
2015-04-25 22:14:13 +0200

    funciona

--- rankall.R
+++ rankall.R
@@ -12,25 +12,26 @@ rankall <- function(outcome, num = "best") {
     if (! outcome %in% names(cases)) {
         stop("invalid outcome")
     }
-    
-    # obtain all states
-    states <- unique(data[["State"]])
-    
+        
     # filter all hospitals by outcome
     outcome_data <- subset(data, data[[cases[[outcome]]]] != "Not Available")
     
     # keep only columns "State" "Hospital name" and "outcome rate"
-    short_outcome_data <- outcome_data[c(2, 7, cases[[outcome]])]
-    # short_outcome_data <- table(outcome_data[["State"]], outcome_data[["Hospital.Name"]], outcome_data[[cases[[outcome]]]])
+    short_outcome_data <- outcome_data[c(7, 2, cases[[outcome]])]
     
     # Rank hospitals based on outcome rate per state
-    sorted_hospitals <- tapply(short_outcome_data[[cases[[outcome]]]], states, 
-                               function(x) x[order(x, short_outcome_data[["Hospital.Name"]])] )
-    
+    ## sorted_hospitals <- tapply(short_outcome_data[[cases[[outcome]]]], states, 
+    ##                           function(x) x[order(as.numeric(x[[cases[[outcome]]]]), tapply(x[["Hospital.Name"]])] )
+    # no vale, estamos usando el indice viejo, que se sale de la matriz recortada
+    #sorted_outcomes <- tapply(short_outcome_data[[3]], states, function(x) x[order(as.numeric(x[[3]]))])
+    #rank_hospital <- tapply(sorted_outcomes, states, function(y) )
+    sorted_hospitals <- short_outcome_data[order(short_outcome_data[[1]], as.numeric(short_outcome_data[[3]]), short_outcome_data[[2]]),]
+
     # Select hospital in selected position
     hospital_position <- function(data, num){
-        position <- list("best" = 1, "worst" = nrow(data))
+        position <- list("best" = 1, "worst" = length(data))
         # if "best" or "worst" are used
+        rank <- num
         if (! is.numeric(num)) {
             rank <- position[[num]]
         } 
@@ -38,18 +39,22 @@ rankall <- function(outcome, num = "best") {
             # if the position in the ranking is out of the list
             if (num > position[["worst"]] ) {
                 return("NA")
-            } 
+            }
         }
+        
+        # if the position in the ranking is out of the list
+        # if a is a vector, and N > length(a), a[N] = NA, so no need to control it
         result <- data[rank]    
     }
-    ranked_hospitals <-tapply(sorted_hospitals, states, rank)
+    #ranked_hospitals <-tapply(sorted_hospitals, states, rank)
+    ranked_hospitals <- tapply(sorted_hospitals[[2]], sorted_hospitals[[1]], hospital_position, num = num)
     
-    # format output
-    result <- table(ranked_hospitals[["Hospital Name"]], ranked_hospitals[["State"]])
-    colnames(result) <- c("hospital", "states")
-    rownames(result) <- states
+    # format output, shape, column names, and type (data.frame)
+    result <- t(rbind(ranked_hospitals, rownames(ranked_hospitals)))
+    colnames(result) <- c("hospital", "state")
+    result <- data.frame(result)
+    return(result)
     
-    # rankall <- data.frame(hospital = "<NA>", state = states, row.names = states)
 }
 
 # > source("rankall.R")
